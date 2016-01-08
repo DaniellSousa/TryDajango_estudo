14ebe5aaeb4ab1201fa500f09b73fbce8bd45bc6
Daniel <daniel.monteiro@deway.com.br>
2016-01-08 17:31:25 -0300

    Try Django 1.9 - 25 of 38 - Setup Static Files - CSS & Javascript & Images in Django

--- src/posts/views.py
+++ src/posts/views.py
@@ -14,8 +14,7 @@ def post_create(request):
 		instance.save()
 		messages.success(request, "Successfully Created")
 		return HttpResponseRedirect(instance.get_absolute_url())
-	else:
-		messages.error(request, "Not Successfully Created")
+
 	context = {
 		"form": form,
 	}
@@ -37,7 +36,7 @@ def post_list(request):
 		"title":"List",
 	}		
 
-	return render(request, "index.html", context)
+	return render(request, "post_list.html", context)
 
 def post_update(request, id=None):
 	instance = get_object_or_404(Post, id=id)

054fc518f80184c18552dfc2ba6add7f7f9983d8
Daniel <daniel.monteiro@deway.com.br>
2016-01-08 14:01:55 -0300

    Try Django 1.9 - 19 of 38 - URL Links & Get Absolute URL

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Post
+
+def post_create(request):
+	return HttpResponse("<h1>Hello</h1>")
+
+def post_detail(request, id=None):
+	#instance = Post.objects.get(id=1)
+	instance = get_object_or_404(Post, id=id)
+	context = {
+		"title": instance.title,
+		"instance": instance,
+	}
+	return render(request, "post_detail.html", context)
+
+def post_list(request):
+	queryset = Post.objects.all()
+	context = {
+		"object_list":queryset,
+		"title":"List",
+	}		
+
+#	if request.user.is_authenticated():
+#		context = {
+#			"title":"My User List"
+#		}
+#	else:
+#		context = {
+#			"title":"List"
+#		}
+
+	return render(request, "index.html", context)
+
+def post_update(request):
+	return HttpResponse("<h1>Update</h1>")
+
+def post_delete(request):
+	return HttpResponse("<h1>Delete</h1>")
--- src/trydjango19/settings.py
+++ src/trydjango19/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = "/Users/daniel/Desktop/trydjango19/src"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'trydjango19.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

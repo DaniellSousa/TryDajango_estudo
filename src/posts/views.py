103183f0bb3c1f52f64a8455e51afc7bf239c38f
Daniel <daniel.monteiro@deway.com.br>
2016-01-11 09:57:50 -0300

    Try Django 1.9 - 27 of 38 - Pagination by QuerySet completado

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,6 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -30,10 +32,23 @@ def post_detail(request, id=None):
 	return render(request, "post_detail.html", context)
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all() #.order_by("-timestamp")
+	paginator = Paginator(queryset_list, 25)
+
+	page_request_var = "page"
+	page = request.GET.get(page_request_var)
+
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		queryset = paginator.page(1)
+	except EmptyPage:
+		queryset = paginator.page(paginator.num_pages)
+
 	context = {
 		"object_list":queryset,
 		"title":"List",
+		"page_request_var":page_request_var
 	}		
 
 	return render(request, "post_list.html", context)
